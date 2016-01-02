3ea777b4420e2ce15b265b898d27b642f5d7a591
Pomax <pomax@nihongoresources.com>
2016-01-01 16:40:41 -0800

    .

--- test.js
+++ test.js
@@ -75,3 +75,15 @@ assert.equal(
 ].forEach(function(b) {
   assert.equal(b.toString(), "[0/0, 0/1, 1/1, 1/2, 2/2]");
 });
+
+[
+  new Bezier([{x:0,y:0}, {x:0,y:1}, {x:1,y:1}, {x:1,y:2}, {x:2,y:2}, {x:2,y:3}]),
+].forEach(function(b) {
+  assert.equal(b.toString(), "[0/0, 0/1, 1/1, 1/2, 2/2, 2/3]");
+});
+
+[
+  new Bezier([{x:0,y:0,z:10}, {x:0,y:1,z:11}, {x:1,y:1,z:12}, {x:1,y:2,z:13}, {x:2,y:2,z:14}, {x:2,y:3,z:15}]),
+].forEach(function(b) {
+  assert.equal(b.toString(), "[0/0/10, 0/1/11, 1/1/12, 1/2/13, 2/2/14, 2/3/15]");
+});
