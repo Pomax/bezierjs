ca4350521ffce9a74b8a87916bc85a2ac904a3dd
Pomax <pomax@nihongoresources.com>
2016-02-04 11:00:00 -0800

    fix for IEEE rounding errors past the significant digit

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 var Bezier = require("./lib");
+var utils = Bezier.getUtils();
 var assert = require("chai").use(require("chai-stats")).assert;
 
 // plain and SVG quadratic check
@@ -117,5 +118,33 @@ var test_line = { p1: { x: -.56, y: .95}, p2: { x: .57, y: .95 } };
 assert(new Bezier(test_bezier).intersects(test_line).length !== 0);
 
 
+// test for numerical precision despite rounding errors after the
+// significant decimal.
+(function() {
+  var p = [
+        {x:0, y:1.74},
+        {x:0.21, y:1.67},
+        {x:0.28, y:1.32},
+        {x:0.28, y:0.86}
+      ],
 
+      t = 0.9336954111478684,
+      mt = 1-t,
+      mt2 = mt*mt,
+      t2 = t*t,
 
+      a = mt2*mt,
+      b = mt2*t*3,
+      c = mt*t2*3,
+      d = t*t2,
+
+      np = {
+        x: a*p[0].x + b*p[1].x + c*p[2].x + d*p[3].x,
+        y: a*p[0].y + b*p[1].y + c*p[2].y + d*p[3].y
+      },
+
+      my = 0.95,
+      MY = 0.95;
+
+  assert(utils.between(np.y,my,MY), true, "y inside range, despite IEEE rounding");
+}());
