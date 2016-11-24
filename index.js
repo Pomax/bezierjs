41c76905affb6c823d7a7c8b785ec65fd3542e5f
Louis-Rémi Babé <lrbabe@gmail.com>
2016-11-24 18:38:30 +0100

    initial commit of bezier.fp.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./lib/bezier');
+// Solve cross-dependencies between bezier.js and utils.js
+var _utils   = {};
+var Bezier   = require('./lib/bezier.js')(_utils);
+var utils    = require('./lib/utils.js')(Bezier);
+
+Object.assign(_utils, utils);
+
+module.exports = Bezier;
