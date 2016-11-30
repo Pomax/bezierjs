6882133dcf44ad1a67d8e4f3feae8be68fdfd0d6
Alexis Launay <yorunohikage@gmail.com>
2016-11-30 18:08:33 +0100

    Rewrite functional files without encapsulation

--- index.fp.js
+++ index.fp.js
@@ -1,13 +1,1 @@
-require('./lib/assign-polyfill');
-// Solve cross-dependencies between bezier.js and utils.js
-var _utils   = {};
-var _bezier  = {};
-var Bezier   = require('./lib/bezier.js')(_utils);
-var utils    = require('./lib/utils.js')(_bezier);
-var BezierFP = require('./lib/bezier.fp.js')(_utils);
-var utilsFP  = require('./lib/utils.fp.js')(_bezier);
-
-Object.assign(_utils, utils, utilsFP);
-Object.assign(_bezier, Bezier, BezierFP);
-
-module.exports = _bezier;
+module.exports = require('./lib/bezier.fp');
