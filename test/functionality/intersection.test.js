15ac13b805b6067d2a5d62c5ec76b0b0174aab5d
ShaMan123 <shacharnen@gmail.com>
2024-01-21 10:03:50 +0200

    feat(): `intersectsLine` line type
    fix

--- test/functionality/intersection.test.js
+++ test/functionality/intersection.test.js
@@ -1,12 +1,44 @@
 import { Bezier } from "../../src/bezier.js";
 
 describe(`Intersection testing`, () => {
+  test.each(["line", "ray", "segment", undefined])(
+    `line(%s)/curve intersection`,
+    (type) => {
+      const b = new Bezier(76, 250, 77, 150, 220, 50);
+      const line = { p1: { x: 13, y: 140 }, p2: { x: 213, y: 140 }, type };
+      const intersections = b.intersects(line);
+
+      expect(intersections.length).toBe(1); // curve intersects horizontal
+      expect(intersections[0]).toBe(0.55); // curve intersects horizontal
+    }
+  );
+
   test(`line/curve intersection`, () => {
-    var b = new Bezier(76, 250, 77, 150, 220, 50);
-    var line = { p1: { x: 13, y: 140 }, p2: { x: 213, y: 140 } };
-    var intersections = b.intersects(line);
+    const b = new Bezier(76, 250, 77, 150, 220, 50);
+    const line = { p1: { x: 13, y: 140 }, p2: { x: 14, y: 140 }, type: "line" };
+    const line2 = {
+      p1: { x: 14, y: 140 },
+      p2: { x: 13, y: 140 },
+      type: "line",
+    };
+    const intersections = b.intersects(line);
+    const intersections2 = b.intersects(line2);
 
     expect(intersections.length).toBe(1); // curve intersects horizontal
     expect(intersections[0]).toBe(0.55); // curve intersects horizontal
+    expect(intersections2.length).toBe(1); // curve intersects horizontal
+    expect(intersections2[0]).toBe(0.55); // curve intersects horizontal
+  });
+
+  test(`ray/curve intersection`, () => {
+    const b = new Bezier(76, 250, 77, 150, 220, 50);
+    const line = { p1: { x: 13, y: 140 }, p2: { x: 14, y: 140 }, type: "ray" };
+    const line2 = { p1: { x: 14, y: 140 }, p2: { x: 13, y: 140 }, type: "ray" };
+    const intersections = b.intersects(line);
+    const intersections2 = b.intersects(line2);
+
+    expect(intersections.length).toBe(1); // curve intersects horizontal
+    expect(intersections[0]).toBe(0.55); // curve intersects horizontal
+    expect(intersections2.length).toBe(0); // curve intersects horizontal
   });
 });
